528cc9a8d76f3e51e7ebeca48f13b14e2c572f3a
Willem Olding <willemolding@gmail.com>
2018-06-14 17:17:56 -0600

    adds anchor zome and api to voting

--- dna/voting/voting.js
+++ dna/voting/voting.js
@@ -67,6 +67,23 @@ function reduceState(initialState, votesL, votesR) {
     }
 }
 
+/*----------  Anchor API  ----------*/
+
+function anchor(anchorType, anchorText) {
+  return call('anchors', 'anchor', {
+    anchorType: anchorType,
+    anchorText: anchorText
+  }).replace(/"/g, '');
+}
+
+
+function anchorExists(anchorType, anchorText) {
+  return call('anchors', 'exists', {
+    anchorType: anchorType,
+    anchorText: anchorText
+  });
+}
+
 /*=====  End of Local Zome Functions  ======*/
 
 
