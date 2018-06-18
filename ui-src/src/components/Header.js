40e5730895cf39e9a74b4d316220b7829a617ee2
Lisa Jetton <ldjetton21@gmail.com>
2018-06-18 15:21:31 -0600

    rough outlines for ui components

---
+++ ui-src/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends React.Component {
+	render() {
+		return (
+			<div className="Header">
+				<h1>Welcome to Post PongChain</h1>
+			</div>
+		);
+	}
+}
+
+
+export default Header;
