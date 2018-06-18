93d4e389e970a596d298a0d582cb03d6afb71531
Michael Dougherty <maackle.d@gmail.com>
2018-06-18 16:42:06 -0600

    Adds VoteButton & stories

--- ui-src/src/components/buttonController/buttonController.js
+++ ui-src/src/components/buttonController/buttonController.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import "./buttonController.css";
+import React, { Component, PropTypes } from "react";
+import "./ButtonController.css";
 
+import VoteButton from './VoteButton';
 
-class buttonController extends Component {
-	static propTypes = {
-		upCount: React.PropTypes.number,
-		stayCount: React.PropTypes.number,
-		downCount: React.PropTypes.number
-	};
+class ButtonController extends Component {
+	// static propTypes = {
+	// 	upCount: PropTypes.number,
+	// 	stayCount: PropTypes.number,
+	// 	downCount: PropTypes.number
+	// };
 
 	// static defaultProps = {
 	// 	upCount: 0,
@@ -17,41 +18,38 @@ class buttonController extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {
-			upCount: 0,
-			stayCount: 0,
-			downCount: 0
-		};
 	}
 
 
 	handleInputChange = event => {
-	    const { name, value } = event.target;   
+	    let { name, value } = event.target;
 	    if (name === "upCount"){
 	    	value += 1
 	    }
 	   	else if (name === "downCount"){
 	    	value -= 1
 	    }
-	    this.setState({
-	      [name]: value
-	    });
+	    console.log('TODO: action: ', value)
 	}
 
 	render() {
 		return (
 			<div>
-				<ul>
-					<li className="button" bsStyle="success" bsSize="large" value={this.state.upCount} onclick={this.handleChange}>
-						Up
-					</li>
-					<li className="button" bsStyle="warning" bsSize="large" value={this.state.stayCount} onclick={this.handleChange}>
-						Stay
+				<ul className="no-bullets">
+					<li>
+						<VoteButton handleVote={this.handleChange}>
+							Up
+						</VoteButton>
 					</li>
 					<li>
-						<button className="button" bsStyle="danger" bsSize="large" value={this.state.downCount} onclick={this.handleChange}>
+						<VoteButton handleVote={this.handleChange}>
+							Stay
+						</VoteButton>
+					</li>
+					<li>
+						<VoteButton handleVote={this.handleChange}>
 							Down
-						</button>
+						</VoteButton>
 					</li>
 				</ul>
 			</div>
@@ -59,5 +57,4 @@ class buttonController extends Component {
 	}
 }
 
-export default buttonController;
-
+export default ButtonController;
--- ui-src/src/stories/index.js
+++ ui-src/src/stories/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Header from '../components/Header';
 
-storiesOf('Header', module)
-  .add('with text', () => (
-    <Header>
-    	<h1>Welcome to Post PongChain</h1>
-    </Header>
-  ));
+import VoteButton from '../components/ButtonController/VoteButton';
+import ButtonController from '../components/ButtonController';
+
+
+storiesOf('VoteButton', module)
+  .add('default', () =>
+    <VoteButton active={false} disabled={false}>Default</VoteButton>
+  )
+  .add('disabled', () =>
+    <VoteButton active={false} disabled={true}>Active</VoteButton>
+  )
+  .add('active & disabled', () =>
+    <VoteButton active={true} disabled={true}>A & D</VoteButton>
+  )
+
+
+storiesOf('ButtonController', module)
+  .add('default state', () =>
+    <ButtonController />
+  );
+
