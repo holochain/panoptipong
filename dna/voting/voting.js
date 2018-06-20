cf6fee0f2bc88f913e60ab2455644677904c8aad
Michael Dougherty <maackle.d@gmail.com>
2018-06-20 13:25:42 -0600

    New pure reducer

--- dna/voting/voting.js
+++ dna/voting/voting.js
@@ -7,16 +7,12 @@
 
 
 function getState() {
-  return reduceState(initialState, getVoteList('L').map(function(elem){
-    return elem.Entry;
-  }), getVoteList('R').map(function(elem){
-    return elem.Entry;
-  }));
+  var sortedVotes = getVoteList('L').concat(getVoteList('R')).sort(compareVotes);
+  return calcState(initialState, sortedVotes);
 }
 
-//REGESTERD YOUR AGENT
+// REGISTERED YOUR AGENT
 function register() {
-
 
   // get the number of agents in each team so far
   var membersL = getLinks(anchor('members', 'L'), '');
@@ -107,11 +103,14 @@ var initialState = {
       y: 50
   },
   paddleL: 50,
-  paddleR: 50
+  paddleR: 50,
+  scoreL: 0,
+  scoreR: 0,
+  ballMovingLeft: initialBallVelocity.x < 0,
 };
 
 
-function reduceState(initialState, votesL, votesR) {
+function oldCalcState(initialState, votesL, votesR) {
 
     var paddleL =  votesL.reduce(function(acc, elem) {
         return acc + vPaddle * (elem.move / elem.teamL.playerCount);
@@ -138,6 +137,68 @@ function reduceState(initialState, votesL, votesR) {
     };
 }
 
+function calcState(initialState, sortedVotes) {
+
+  function isCollision(paddleY, ballY) {
+    paddleY = mod(paddleL, boardParams.height);
+    ballY = unwrapBallPos(ball.y, boardParams.height);
+    var h = boardParams.paddleHeight;
+    return ballY <= paddleY + h/2 && ballY >= paddleY - h/2;
+  }
+
+  function reduceState(state, vote) {
+    var teamID = vote.teamID;
+    var totalPlayers = vote.teamL.playerCount + vote.teamR.playerCount;
+    var paddleL = state.paddleL;
+    var paddleR = state.paddleR;
+    var scoreL = state.scoreL;
+    var scoreR = state.scoreR;
+    var ball = {
+      x : state.ball.x + initialBallVelocity.x / totalPlayers,
+      y : state.ball.y + initialBallVelocity.y / totalPlayers
+    }
+
+    if (vote.teamID === 'L') {
+      paddleL += vPaddle * (vote.move / vote.teamL.playerCount);
+    } else {
+      paddleR += vPaddle * (vote.move / vote.teamR.playerCount);
+    }
+
+    var ballMovingLeft = Boolean(Math.floor(ball.x / width) % 2);
+    if (ballMovingLeft !== state.ballMovingLeft) {
+      if (!ballMovingLeft && !isCollision(paddleL, ball.y)) {
+        scoreR += 1;
+      } else if (ballMovingLeft && !isCollision(paddleR, ball.y)) {
+        scoreL += 1;
+      }
+    }
+    return {
+      ball: ball,
+      paddleL: paddleL,
+      paddleR: paddleR,
+      scoreL: scoreL,
+      scoreR: scoreR,
+      ballMovingLeft: ballMovingLeft,
+    }
+  }
+
+  function compareVotes(a, b) {
+    var totalVotesA = a.teamL.voteCount + a.teamR.voteCount;
+    var totalVotesB = b.teamL.voteCount + b.teamR.voteCount;
+    if (totalVotesA === totalVotesB) {
+      return makeHash('vote', a) < makeHash('vote', b) ? -1 : 1
+    } else {
+      return totalVotesA - totalVotesB;
+    }
+  }
+
+  sortedVotes.reduce(reduceState, initialState)
+}
+
+function voteStamp(vote) {
+  var totalVotes = vote.teamL.voteCount + vote.teamR.voteCount;
+  return String(totalVotes) + makeHash('vote', vote);
+}
 
 function mod(n, m) {
   return ((n % m) + m) % m;
