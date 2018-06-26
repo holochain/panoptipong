6ac450d6ba70434032b5940a6c2d6b1e8a73693d
Willem Olding <willemolding@gmail.com>
2018-06-25 21:02:22 -0600

    fucken vote buckets

---
+++ dna/voting/src/callbacks.js
@@ -0,0 +1,45 @@
+
+
+// Cast you first Vote and save it localy
+
+function genesis() {
+  return true;
+}
+
+
+function validatePut(entry_type,entry,header,pkg,sources) {
+  return validateCommit(entry_type,entry,header,pkg,sources);
+}
+function validateCommit(entry_type,entry,header,pkg,sources) {
+    return true;
+}
+
+function validateLink(linkingEntryType,baseHash,linkHash,pkg,sources){
+  return true;
+}
+function validateMod(entry_type,hash,newHash,pkg,sources){
+  return true;
+}
+function validateDel(entry_type,hash,pkg,sources) {
+  return true;
+}
+function validatePutPkg(entry_type) {
+  return null;
+}
+function validateModPkg(entry_type) {
+  return null;
+}
+function validateDelPkg(entry_type) {
+  return null;
+}
+function validateLinkPkg(entry_type) {
+  return null;
+}
+
+function validateLink(entryType, hash, links, pkg, sources) {
+  return true;
+}
+
+function validateDelPkg (entryType) {
+return null;
+}
