90dbb19b7ba2f799717402098a3b8f3ce5870047
Willem Olding <willemolding@gmail.com>
2018-06-26 15:41:24 -0600

    fixes type not declaring result

--- dna/voting/src/voting.js
+++ dna/voting/src/voting.js
@@ -12,7 +12,25 @@ function getState() {
   return getBucketState(getCurrentBucket());
 }
 
+function getVotesAfterVote(payload) {
 
+  var sortedVotes = getLinks(makeHash('gameBucket', getCurrentBucket()), 'vote', {Load: true})
+    .map(function (item) { return item.Entry })
+    .sort(compareVotes);
+
+  if(!payload.vote) {
+    // function called with no vote parameter
+    // return the n most recent votes
+    var n = 10;
+    var startIndex = Math.min(sortedVotes.length - n, 0);
+    return sortedVotes.slice(startIndex, sortedVotes.length);
+  } else {
+    // function called with a vote. Return votes ranked after by vote stamp
+    return sortedVotes.filter(function(element) {
+      return compareVotes(payload.vote, element) > 0;
+    });
+  }
+}
 
 function compareVotes(a, b) {
   var totalVotesA = a.teamL.voteCount + a.teamR.voteCount;
@@ -25,8 +43,14 @@ function compareVotes(a, b) {
   }
 }
 
+function getPlayer(payload) {
+
+}
+
 // REGISTERED YOUR AGENT
-function register() {
+function register(payload) {
+
+  var name = payload.name;
 
   // get the number of agents in each team so far
   var membersL = getLinks(anchor('members', 'L'), '');
@@ -49,7 +73,7 @@ function register() {
   } else {
     team = 'R';
   }
-  joinTeam(team);
+  joinTeam(team, name);
   return team;
 }
 
@@ -60,7 +84,7 @@ function getTeam() {
       Entries: true
     },
     Constrain: {
-      EntryTypes: ["teamDesignation"],
+      EntryTypes: ["privatePlayerRegistration"],
       Count: 1
     }
   });
@@ -75,8 +99,8 @@ function vote(payload) {
   var nPlayersL = getLinks(anchor('members', 'L'), '').length;
   var nPlayersR = getLinks(anchor('members', 'R'), '').length;
 
-  var nVotesL =countVotes("L");
-  var nVotesR =countVotes("R");
+  var nVotesL = countVotes("L");
+  var nVotesR = countVotes("R");
 
   var vote = {
     move: move,
@@ -233,7 +257,7 @@ function setCachedBucket(bucket, prevBucket) {
 }
 
 function getCachedBucket(hash) {
-  result=query({
+  var result = query({
     Constrain: {
       EntryTypes: ['cachedGameBucket']
     }
@@ -276,10 +300,18 @@ function getVoteList(teamID) {
 
 
 
-function joinTeam(team) {
-  commit("teamDesignation", team);
+function joinTeam(team, name) {
+  var regoHash = commit("playerRegistration", {teamID: team, agentHash: App.Key.Hash, name: name});
+  commit("privatePlayerRegistration", {teamID: team, agentHash: App.Key.Hash, name: name});
+ 
   var teamAnchorHash = anchor('members', team);
   commit("teamLink", {
     Links: [{ Base: teamAnchorHash, Link: App.Key.Hash, Tag: "" }]
   });
+
+  var playersAnchorHash = anchor('players', 'players');
+  commit("teamLink", {
+    Links: [{ Base: playersAnchorHash, Link: regoHash, Tag: "" }]
+  });
+
 }
--- dna/voting/voting.js
+++ dna/voting/voting.js
@@ -73,6 +73,26 @@ function getState() {
   return getBucketState(getCurrentBucket());
 }
 
+function getVotesAfterVote(payload) {
+
+  var sortedVotes = getLinks(makeHash('gameBucket', getCurrentBucket()), 'vote', { Load: true }).map(function (item) {
+    return item.Entry;
+  }).sort(compareVotes);
+
+  if (!payload.vote) {
+    // function called with no vote parameter
+    // return the n most recent votes
+    var n = 10;
+    var startIndex = Math.min(sortedVotes.length - n, 0);
+    return sortedVotes.slice(startIndex, sortedVotes.length);
+  } else {
+    // function called with a vote. Return votes ranked after by vote stamp
+    return sortedVotes.filter(function (element) {
+      return compareVotes(payload.vote, element) > 0;
+    });
+  }
+}
+
 function compareVotes(a, b) {
   var totalVotesA = a.teamL.voteCount + a.teamR.voteCount;
   var totalVotesB = b.teamL.voteCount + b.teamR.voteCount;
@@ -84,8 +104,12 @@ function compareVotes(a, b) {
   }
 }
 
+function getPlayer(payload) {}
+
 // REGISTERED YOUR AGENT
-function register() {
+function register(payload) {
+
+  var name = payload.name;
 
   // get the number of agents in each team so far
   var membersL = getLinks(anchor('members', 'L'), '');
@@ -112,7 +136,7 @@ function register() {
   } else {
     team = 'R';
   }
-  joinTeam(team);
+  joinTeam(team, name);
   return team;
 }
 
@@ -122,7 +146,7 @@ function getTeam() {
       Entries: true
     },
     Constrain: {
-      EntryTypes: ["teamDesignation"],
+      EntryTypes: ["privatePlayerRegistration"],
       Count: 1
     }
   });
@@ -253,31 +277,10 @@ function getBucketState(bucket) {
   var sortedVotes = getLinks(makeHash('gameBucket', bucket), 'vote', { Load: true }).map(function (item) {
     return item.Entry;
   }).sort(compareVotes);
-  var initialBucketState = updateInitialState(bucket);
+  var initialBucketState = JSON.parse(JSON.stringify(initialState)); // TODO: actually make a fresh new state for each anchor based on hash
   initialBucketState.scoreL = bucket.scoreL;
   initialBucketState.scoreR = bucket.scoreR;
   return calcState(initialBucketState, sortedVotes, boardParams);
-}
-
-function updateInitialState(bucket) {
-  var bucketHash = makeHash(bucket);
-  var ballPositionDelta = hashToIntInRange(bucketHash);
-  var newState = {ball: {
-    x: 60 += ballPositionDelta,
-    y: 50 += ballPositionDelta
-  },
-  paddleL: 50,
-  paddleR: 50,
-  scoreL: 0,
-  scoreR: 0,
-  ballMovingLeft: boardParams.vBallx < 0};
-  return newState;
-}
-
-function hashToIntInRange(hash) {
-  var range = [-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  var index = hash % range.length;
-  return range[index];
 }
 
 function getCurrentBucket(_currentBucket) {
@@ -293,7 +296,7 @@ function getCurrentBucket(_currentBucket) {
       scoreR: state.scoreR,
       gameID: currentBucket.gameID
     };
-    
+
     commit('gameBucket', nextBucket); // in case we are the first person to notice the score
     setCachedBucket(nextBucket, currentBucket); // TODO actually make work
     return getCurrentBucket(nextBucket);
@@ -307,7 +310,7 @@ function setCachedBucket(bucket, prevBucket) {
 }
 
 function getCachedBucket(hash) {
-  result = query({
+  var result = query({
     Constrain: {
       EntryTypes: ['cachedGameBucket']
     }
@@ -347,10 +350,17 @@ function getVoteList(teamID) {
   return voteLinks;
 }
 
-function joinTeam(team) {
-  commit("teamDesignation", team);
+function joinTeam(team, name) {
+  var regoHash = commit("playerRegistration", { teamID: team, agentHash: App.Key.Hash, name: name });
+  commit("privatePlayerRegistration", { teamID: team, agentHash: App.Key.Hash, name: name });
+
   var teamAnchorHash = anchor('members', team);
   commit("teamLink", {
     Links: [{ Base: teamAnchorHash, Link: App.Key.Hash, Tag: "" }]
+  });
+
+  var playersAnchorHash = anchor('players', 'players');
+  commit("teamLink", {
+    Links: [{ Base: playersAnchorHash, Link: regoHash, Tag: "" }]
   });
 }
