092386d4be13b406087bc9e9feb5efaad55f4946
Lisa Jetton <ldjetton21@gmail.com>
2018-06-27 16:44:34 -0600

    bubble action import update

--- ui-src/src/components/PlayersTable.js
+++ ui-src/src/components/PlayersTable.js
@@ -3,29 +3,8 @@ import {connect} from 'react-redux';
 import {updateNameEntry, getPlayers} from '../actions'
 import {Jdenticon} from './Common';
 import './PlayersTable.css';
-//
-// const columns = [{
-//   dataField: "nameEntry",
-//   text: "Player Name"
-// }, {
-//   dataField: "Avatar",
-//   text: "Player Avatar"
-// }];
 
-// const CaptionElement = () => <h3 style={{ borderRadius: '0.25em', textAlign: 'center', color: 'purple', border: '1px solid purple', padding: '0.5em' }}>Team {teamId} Players</h3>;
 const Avatar = ({name}) => <Jdenticon size="30px" hash={name} />
-// const teamIdR = this.props.players.teamId === "R";
-// const teamIdL = this.props.players.teamId === "L";
-//
-// const DrawPlayerTable = () => {
-//   const teamId = this.props.players.teamId;
-//   return (
-//   <div className="playerTable">
-//     {this.props.players.map( player => (
-//       <BootstrapTable keyField="id" data={ player.nameEntry, <Avatar /> } caption={<CaptionElement />} columns={ columns } />
-//     ))}
-//   </div>
-// )}
 
 export default class PlayersTable extends Component {
   constructor(props) {
@@ -52,11 +31,11 @@ export default class PlayersTable extends Component {
     return <div className="PlayersTable">
       <div className="tableWrapper">
         <ul>
-          <li><h3>{playersLeft.length} players</h3></li>
+          <li><h3> Team Left: {playersLeft.length} players</h3></li>
           {playersLeft}
         </ul>
         <ul>
-          <li><h3>{playersRight.length} players</h3></li>
+          <li><h3>Team Right: {playersRight.length} players</h3></li>
           {playersRight}
         </ul>
       </div>
