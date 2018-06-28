a49c8c56715be6aa98c75d4b47ef2151c0badc5c
Michael Dougherty <maackle.d@gmail.com>
2018-06-28 00:16:42 -0600

    Test of getLinks across holochains

--- puretest/index.js
+++ puretest/index.js
@@ -2,32 +2,48 @@
 require('babel-polyfill');
 const test = require('tape');
 
+const fakeVote = salt => ({
+  "move" : -1,
+  "teamL": 0,
+  "teamR": 0,
+  "agentHash": "fake",
+  "randomSalt": salt,
+  "teamID": "L"
+})
+
 test('simple test', t => {
-  const hash = makeHash('vote', {
-      "move" : -1,
-      "teamL": 0,
-      "teamR": 0,
-      "agentHash": "iausdhfouishdf",
-      "randomSalt": "089hw",
-      "teamID": "L"
-  });
+  scenario(2, (alice, bob) => {
+    alice(() => {
+      commit('vote', {
+        "move" : -1,
+        "teamL": 0,
+        "teamR": 0,
+        "agentHash": "iausdhfouishdf",
+        "randomSalt": "089hw",
+        "teamID": "L"
+      })
+    })
 
-  const hash1 = commit('vote', {
-      "move" : -1,
-      "teamL": 0,
-      "teamR": 0,
-      "agentHash": "iausdhfouishdf",
-      "randomSalt": "089hw",
-      "teamID": "L"
+    bob(() => {
+      const anc = anchor("L", "GameID")
+      for (let i=0; i < 100; i++) {
+        const voteHash = commit('vote', fakeVote(''+i))
+        commit('voteLink', {
+          Links: [{ Base: anc, Link: voteHash, Tag: 'vote' }]
+        });
+      }
+      const links = getLinks(anc, '')
+      t.equal(links.length, 100)
+    })
+
+    bob(() => {
+      const anc = anchor("L", "GameID")
+      setTimeout(() => {
+        const links = getLinks(anc, '')
+        t.equal(links.length, 100)
+        t.end()
+      }, 500)
+    })
   })
 
-  t.equal(hash, hash1);
-
-  new Promise(function(fulfill, reject) {
-    t.equal(2,2);
-    setTimeout(fulfill, 2000)
-  }).then(function(v) {
-    t.equal(3,3);
-    t.end()
-  })
 });
